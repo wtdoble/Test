b0827a81dcdf0f41efca8e44d74e930e4f8f29e5
William Doble <wtdoble@hotmail.com>
2024-08-12 11:03:43 -0400

    debugging

--- ginkgo-e2e/utils/setup_utils.go
+++ ginkgo-e2e/utils/setup_utils.go
@@ -24,31 +24,31 @@ import (
  * If the file is not found, it will assume the tests are running in a Kubernetes cluster and use the in-cluster configuration.
  */
 func SetupKubernetesClient() (*kubernetes.Clientset, *rest.Config, error) {
-  var kubeconfig *string
-  if home := homedir.HomeDir(); home != "" {
-    kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-  } else {
-    kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-  }
-  flag.Parse()
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
 
-  fmt.Println(kubeconfig)
+	fmt.Println(kubeconfig)
 
-  cfg, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-  	fmt.Println(err)
-  if err != nil {
-    cfg, err = rest.InClusterConfig()
-    if err != nil {
-      return nil, nil, err
-    }
-  }
-  
-  client, err := kubernetes.NewForConfig(cfg)
-  if err != nil {
-    return nil, nil, err
-  }
+	cfg, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	fmt.Println(err)
+	if err != nil {
+		cfg, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, nil, err
+		}
+	}
 
-  return client, cfg, nil
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client, cfg, nil
 }
 
 func ReadFileContent(filename string) ([]byte, error) {

b43a3cbfcc23935b84688e0eb31f4016b010ae8d
William Doble <wtdoble@hotmail.com>
2024-08-19 09:47:05 -0400

    changed pool to ubuntu

--- ginkgo-e2e/wtdTest/test_suite_test.go
+++ ginkgo-e2e/wtdTest/test_suite_test.go
@@ -134,10 +134,20 @@ var _ = Describe("Test", func() {
 			}
 		}
 
-		fmt.Println("Error test")
-		fmt.Println("Err test")
-		fmt.Println("Warning test")
-		fmt.Println("Warn test")
+		// var Reset = "\033[0m"
+		// var Red = "\033[31m"
+		// var Green = "\033[32m"
+		// var Yellow = "\033[33m"
+		// var Blue = "\033[34m"
+		// var Magenta = "\033[35m"
+		// var Cyan = "\033[36m"
+		// var Gray = "\033[37m"
+		// var White = "\033[97m"
+
+		fmt.Printf("Error test")
+		// fmt.Println("Err test")
+		// fmt.Println("Warning test")
+		// fmt.Println("Warn test")
 
 		// Expect(_ = err).NotTo(HaveOccurred())
 		// Expect(apiResponse.Data).NotTo(BeNil())

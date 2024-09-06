0bfaf66da853e8be2cf398832ea06a56ab08ca79
William Doble <wtdoble@hotmail.com>
2024-09-06 13:22:31 -0400

    incorporated Grace's comments

--- ginkgo-e2e/wtdTest/test_suite_test.go
+++ ginkgo-e2e/wtdTest/test_suite_test.go
@@ -43,15 +43,45 @@ var _ = AfterSuite(func() {
 	By("tearing down the test environment")
 })
 
-var _ = Describe("Test", func() {
+func readFile(fileName string, podName string) []string {
+	fmt.Printf("Examining %s\r\n", fileName)
+	var cmd []string = []string{"cat", fileName}
+	stdout, _, err := utils.ExecCmd(K8sClient, Cfg, podName, containerName, namespace, cmd)
+	Expect(err).To(BeNil())
 
-	var cmd []string
+	return strings.Split(stdout, "\n")
+}
+
+func writeLines(lines []string) int {
+	count := 0
+	for _, rawLine := range lines {
+		//fmt.Printf("raw line #%d: %s\r\n", i, rawLine)
+		line := strings.Trim(rawLine, " ")
+		if len(line) > 0 {
+			//fmt.Printf("line #%d: %s\r\n", i, line)
+			fmt.Printf("%s\r\n", line)
+			count++
+		} else {
+			fmt.Println("<empty line>")
+		}
+	}
+
+	return count
+}
+
+var _ = Describe("Files Test", func() {
+
+	const mdsdErrFileName = "/opt/microsoft/linuxmonagent/mdsd.err"
+	const mdsdInfoFileName = "/opt/microsoft/linuxmonagent/mdsd.info"
+	const mdsdWarnFileName = "/opt/microsoft/linuxmonagent/mdsd.warn"
+	const metricsExtDebugLogFileName = "/MetricsExtensionConsoleDebugLog.log"
+
+	// // var cmd []string
 	var podName string = ""
 	// var apiResponse utils.APIResponse
 
 	BeforeEach(func() {
-		cmd = []string{}
-
+		// cmd = []string{}
 		v1Pod, err := utils.GetPodsWithLabel(K8sClient, namespace, controllerLabelName, controllerLabelValue)
 		Expect(err).To(BeNil())
 
@@ -65,13 +95,6 @@ var _ = Describe("Test", func() {
 		}
 	})
 
-	type mdsdInfoConfigLine struct {
-		line   string
-		dt     string
-		status string
-		data   string
-	}
-
 	type metricExtConsoleLine struct {
 		line   string
 		dt     string
@@ -79,63 +102,42 @@ var _ = Describe("Test", func() {
 		data   string
 	}
 
-	type LineProcessor func(string) (bool, string)
+	// type LineProcessor func(string) (bool, string)
+	// type TestEvaluator func([]string) bool
 
-	// func mdsdInfoLineProcessor(line string) (bool, string) {
-	// 	line = strings.Trim(line, " ")
-	// 	return len(line) > 0, line
-	// }
+	It("/opt/microsoft/linuxmonagent/mdsd.err Test", func() {
 
-	DescribeTable("/opt/microsoft/linuxmonagent/ files Test",
-		func(fileName string, proc LineProcessor) {
-			Expect(podName).NotTo(BeEmpty())
-			Expect(fileName).NotTo(BeEmpty())
+		Expect(podName).NotTo(BeEmpty())
 
-			//fullFileName := fmt.Sprintf("/opt/microsoft/linuxmonagent/%s", fileName)
-			fmt.Printf("Examining %s\r\n", fileName)
-			cmd = []string{"cat", fileName}
-			stdout, _, err := utils.ExecCmd(K8sClient, Cfg, podName, containerName, namespace, cmd)
-			Expect(err).To(BeNil())
+		numErrLines := writeLines(readFile(mdsdErrFileName, podName))
+		if numErrLines > 0 {
+			writeLines(readFile(mdsdInfoFileName, podName))
+			writeLines(readFile(mdsdWarnFileName, podName))
+		}
+	})
 
-			var lines []string = strings.Split(stdout, "\n")
+	It("/MetricsExtensionConsoleDebugLog.log Test", func() {
 
-			for _, rawLine := range lines {
-				//fmt.Printf("raw line #%d: %s\r\n", i, rawLine)
-				nonEmpty, formattedLine := proc(rawLine)
-				if nonEmpty {
-					//fmt.Printf("line #%d: %s\r\n", i, formattedLine)
-					fmt.Printf("%s\r\n", formattedLine)
-				} else {
-					fmt.Println("<empty line>")
-				}
-			}
-		},
-		// func(fileName string, proc LineProcessor) string {
-		// 	return fmt.Sprintf("Examining /opt/microsoft/linuxmonagent/%s", fileName)
-		// },
+		Expect(podName).NotTo(BeEmpty())
 
-		// Entry("Examine the contents of mdsd.info"),
-		Entry(nil, "/opt/microsoft/linuxmonagent/mdsd.info", func(line string) (bool, string) {
-			line = strings.Trim(line, " ")
-			return len(line) > 0, line
-		}),
-		// Entry("Examine the contents of mdsd.err"),
-		Entry(nil, "/opt/microsoft/linuxmonagent/mdsd.err", func(line string) (bool, string) {
-			line = strings.Trim(line, " ")
-			return len(line) > 0, line
-		}),
-		Entry(nil, "/MetricsExtensionConsoleDebugLog.log", func(line string) (bool, string) {
-			line = strings.Trim(line, " ")
+		var lines []string = readFile(metricsExtDebugLogFileName, podName)
+
+		// for i := 0; i < 10; i++ {
+		// 	line := lines[i]
+		for _, line := range lines {
+			//fmt.Printf("#line: %d, %s \r\n", i, line)
 
 			var fields []string = strings.Fields(line)
 			if len(fields) > 2 {
 				metricExt := metricExtConsoleLine{line: line, dt: fields[0], status: fields[1], data: fields[2]}
-				fmt.Println(metricExt.status)
+				//fmt.Println(metricExt.status)
+				status := strings.ToLower(metricExt.status)
+				if strings.Contains(status, "error") || strings.Contains(status, "warn") {
+					fmt.Println(line)
+				}
 			}
-
-			return len(line) > 0, line
-		}),
-	)
+		}
+	})
 
 	// It("/MetricsExtensionConsoleDebugLog Test", func() {
 	// 	Expect(podName).NotTo(BeEmpty())
